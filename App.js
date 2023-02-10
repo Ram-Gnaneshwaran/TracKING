fc3af2c3e8a3a0ea1d6ebcd988b1dc74bef77a45
Ram Gnaneshwaran <itzgnanesh@gmail.com>
2023-02-10 20:40:13 +0530

    Minor

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
-
-
 import HomeScreen from './screens/HomeScreen';
 import StudentScreen1 from './screens/StudentScreen1';
 import DriverScreen from './screens/DriverScreen';
 
 export default function App() {
   return (
-    // <HomeScreen />
+    <HomeScreen />
     // <StudentScreen1 />
-    <DriverScreen />
+    // <DriverScreen />
   );
 }
 
