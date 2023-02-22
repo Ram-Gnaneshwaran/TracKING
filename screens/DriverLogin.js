777907398babc48d31404d7e0bc089107d4f0371
Ram Gnaneshwaran <itzgnanesh@gmail.com>
2023-02-22 06:36:07 +0530

    Scroll View 2

--- screens/DriverLogin.js
+++ screens/DriverLogin.js
@@ -1,4 +1,4 @@
-import { Image, KeyboardAvoidingView, Text, TextInput, View, Pressable} from 'react-native'
+import { Image, KeyboardAvoidingView, Text, TextInput, View, Pressable, ScrollView} from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StyledComponent } from 'nativewind'
@@ -54,109 +54,112 @@ const DriverLogin = () => {
 
 return (
   // Keyboard Avoid View
-  <KeyboardAvoidingView>
+  <ScrollView >
+    <KeyboardAvoidingView>
 
-  {/* Safe Area View */}
+    {/* Safe Area View */}
     <SafeAreaView className = "flex items-center bg-white h-screen" >
-      {/* Logo */}
-      <Image 
-          className = "mt-10"
-          source={require('/Users/gnaneshwaran/Documents/GitProjects/TracKing/my-app/assets/Logo.png')} 
-          />
+        {/* Logo */}
+        <Image 
+            className = "mt-10"
+            source={require('/Users/gnaneshwaran/Documents/GitProjects/TracKing/my-app/assets/Logo.png')} 
+            />
 
-      {/* Login Form */}
-      <StyledComponent 
-      component = {View} 
-      className =" flex items-center mt-10 px-10 py-16 pt-6 bg-white"
-      style={{
-          borderWidth: 2,
-          borderColor: "black",
-          borderRadius: 4,
-          shadowColor: '#171717',
-          shadowOffset: {width: -5, height: 5},
-          shadowOpacity: 1,
-          shadowRadius: 0,
-          }}
-      >
-          {/* Login Text */}
-          <Text className="text-3xl font-bold" >Login </Text>
+        {/* Login Form */}
+        <StyledComponent 
+        component = {View} 
+        className =" flex items-center mt-10 px-10 py-16 pt-6 bg-white"
+        style={{
+            borderWidth: 2,
+            borderColor: "black",
+            borderRadius: 4,
+            shadowColor: '#171717',
+            shadowOffset: {width: -5, height: 5},
+            shadowOpacity: 1,
+            shadowRadius: 0,
+            }}
+        >
+            {/* Login Text */}
+            <Text className="text-3xl font-bold" >Login </Text>
 
-          {/* Email Input  */}
-          <TextInput 
-              className ="mt-10 bg-white w-52 h-10 px-5"
-              style = {{
-                  borderWidth: 2,
-                  borderColor: "black",
-                  borderRadius: 4,
-              }}
-              placeholder='Email' 
-              value={email} 
-              onChangeText={ text => setEmail(text)} 
-              />
+            {/* Email Input  */}
+            <TextInput 
+                className ="mt-10 bg-white w-52 h-10 px-5"
+                style = {{
+                    borderWidth: 2,
+                    borderColor: "black",
+                    borderRadius: 4,
+                }}
+                placeholder='Email' 
+                value={email} 
+                onChangeText={ text => setEmail(text)} 
+                />
 
-          {/* Password Input  */}
-           <TextInput 
-              className ="mt-10 bg-white w-52 h-10 px-5 "
-              style = {{
-                  borderWidth: 2,
-                  borderColor: "black",
-                  borderRadius: 4,
-              }}
-              placeholder='Password' 
-              secureTextEntry
-              value= {password}
-              onChangeText={ text => setPassword(text)} 
-              />          
+            {/* Password Input  */}
+            <TextInput 
+                className ="mt-10 bg-white w-52 h-10 px-5 "
+                style = {{
+                    borderWidth: 2,
+                    borderColor: "black",
+                    borderRadius: 4,
+                }}
+                placeholder='Password' 
+                secureTextEntry
+                value= {password}
+                onChangeText={ text => setPassword(text)} 
+                />          
 
-          {/* Login Button */}
-          <Pressable onPress={handleLogin}>
-              <StyledComponent 
-              component={View} 
-              className="flex-row px-10 py-4 bg-black mt-10 " 
-                  style={{
-                  borderWidth: 1,
-                  borderColor: "white",
-                  borderRadius: 4,
-                  shadowColor: '#707070',
-                  shadowOffset: {width: -5, height: 5},
-                  shadowOpacity: 1,
-                  shadowRadius: 0,
-                  }}>
-              {/* Button Text */}
-              <Text className="text-xl font-semibold mr-4 text-white" >Login</Text>
-              {/* Button Logo */}
-              <Image 
-                  source={require('/Users/gnaneshwaran/Documents/GitProjects/TracKing/my-app/assets/Login_Icon.png')} 
-                  />
-              </StyledComponent>
-          </Pressable>
+            {/* Login Button */}
+            <Pressable onPress={handleLogin}>
+                <StyledComponent 
+                component={View} 
+                className="flex-row px-10 py-4 bg-black mt-10 " 
+                    style={{
+                    borderWidth: 1,
+                    borderColor: "white",
+                    borderRadius: 4,
+                    shadowColor: '#707070',
+                    shadowOffset: {width: -5, height: 5},
+                    shadowOpacity: 1,
+                    shadowRadius: 0,
+                    }}>
+                {/* Button Text */}
+                <Text className="text-xl font-semibold mr-4 text-white" >Login</Text>
+                {/* Button Logo */}
+                <Image 
+                    source={require('/Users/gnaneshwaran/Documents/GitProjects/TracKing/my-app/assets/Login_Icon.png')} 
+                    />
+                </StyledComponent>
+            </Pressable>
 
-          {/* Register Button */}
-          <Pressable onPress={handleSignUp}>
-              <StyledComponent 
-              component={View} 
-              className="flex-row px-5 py-4 bg-white mt-10 " 
-                  style={{
-                  borderWidth: 1,
-                  borderColor: "black",
-                  borderRadius: 4,
-                  shadowColor: 'black',
-                  shadowOffset: {width: -5, height: 5},
-                  shadowOpacity: 1,
-                  shadowRadius: 0,
-                  }}>
-              {/* Button Text */}
-              <Text className="text-xl font-semibold mr-4 text-black" >Register</Text>
-              {/* Button Logo */}
-              <Image 
-                  source={require('/Users/gnaneshwaran/Documents/GitProjects/TracKing/my-app/assets/Register_Icon.png')} 
-                  />
-              </StyledComponent>
-          </Pressable>
+            {/* Register Button */}
+            <Pressable onPress={handleSignUp}>
+                <StyledComponent 
+                component={View} 
+                className="flex-row px-5 py-4 bg-white mt-10 " 
+                    style={{
+                    borderWidth: 1,
+                    borderColor: "black",
+                    borderRadius: 4,
+                    shadowColor: 'black',
+                    shadowOffset: {width: -5, height: 5},
+                    shadowOpacity: 1,
+                    shadowRadius: 0,
+                    }}>
+                {/* Button Text */}
+                <Text className="text-xl font-semibold mr-4 text-black" >Register</Text>
+                {/* Button Logo */}
+                <Image 
+                    source={require('/Users/gnaneshwaran/Documents/GitProjects/TracKing/my-app/assets/Register_Icon.png')} 
+                    />
+                </StyledComponent>
+            </Pressable>
 
-      </StyledComponent>
+        </StyledComponent>
     </SafeAreaView>
-  </KeyboardAvoidingView>
+    </KeyboardAvoidingView>
+  </ScrollView>
+
 )
 }
 
